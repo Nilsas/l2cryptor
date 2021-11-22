be44425cecda38852273badb05130f3666b11323
Mr. Ebrilo <mr.ebrilo.gitlab@gmail.com>
2021-11-22 17:47:57 +0300

    replaced RSAKey implementation for test

--- rsa-key.go
+++ rsa-key.go
@@ -7,30 +7,3 @@ type RSAKey interface {
 	E() *big.Int // Public exponent
 	D() *big.Int // Private exponent
 }
-
-type rsaKey struct {
-	n *big.Int
-	e *big.Int
-	d *big.Int
-}
-
-func (k *rsaKey) setN(n string) *rsaKey {
-	k.n, _ = new(big.Int).SetString(n, 16)
-	return k
-}
-
-func (k *rsaKey) setE(e string) *rsaKey {
-	k.e, _ = new(big.Int).SetString(e, 16)
-	return k
-}
-
-func (k *rsaKey) setD(d string) *rsaKey {
-	k.d, _ = new(big.Int).SetString(d, 16)
-	return k
-}
-
-func (k *rsaKey) N() *big.Int { return k.n }
-
-func (k *rsaKey) E() *big.Int { return k.e }
-
-func (k *rsaKey) D() *big.Int { return k.d }
